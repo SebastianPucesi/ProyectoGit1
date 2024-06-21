f5a26de61b152693579d3d14e60145f00b280930
Sebastian <jsgaleano@pucesi.edu.ec>
2024-06-20 20:00:59 -0500

    actividad en clase

--- Recursividad.py
+++ Recursividad.py
@@ -1,3 +1,21 @@
-print("Hola")
-print("Cambios")
-print("reset")
+#for i in range (5):
+#    print(i, end=" ,")
+'''
+#Metodo recursivo
+def MostrarNumero(numero):
+    #Caso base
+    if numero <=0:
+        return
+    #Caso recursivo
+    else:
+        print(numero, end=" ,")
+        return MostrarNumero(numero-1)
+
+MostrarNumero(6)'''
+
+def totalPaginas(Libros):
+    if len(Libros) == 1:
+        return Libros [0]
+    else:
+        return Libros[0] + totalPaginas(Libros[1:])
+print(totalPaginas([10,20,30,40]))
